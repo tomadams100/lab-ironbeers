9706ad9f1d14e91500dac7d2bd7301726d7f3ef4
Tom Adams <thomasjeadams@btinternet.com>
2021-11-10 16:32:46 +0100

    finished lab, except for 6 & 7 bonuses

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -14,9 +15,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname,'views/partials'))
 
 // Add the route handlers here:
+app.all("/beers", (req, res)=> {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render("beers",beersFromApi)
+  })
+  .catch(error => console.log(error))
+})
+
+app.all("/random-beer", (req,res)=> {
+  punkAPI
+  .getRandom()
+  .then(responseFromApi => {
+    console.log("The random beer is: ", responseFromApi)
+    res.render("random-beer",responseFromApi)
+  })
+  .catch(error => console.log(error))
+})
 
 app.get('/', (req, res) => {
   res.render('index');
